a257a893a742743ea4620447219cb5c549ef1bf1
romastony0 <romastony0@gmail.com>
2025-01-22 18:06:00 +0530

    master

--- main.js
+++ main.js
@@ -3,9 +3,7 @@ import express from "express";
 const app = express();
 const port = 3000;
 app.get("/", (req, res) => {
-  res.json({
-    message: "Hello World",
-  });
+  res.se
 });
 
 app.listen(port, () => {
