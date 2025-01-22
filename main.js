5d360d16dcf01abc530d4fba03ac9fd8c8697ffb
romastony0 <romastony0@gmail.com>
2025-01-22 18:21:42 +0530

    Crud Started

--- main.js
+++ main.js
@@ -3,7 +3,40 @@ import express from "express";
 const app = express();
 const port = 3000;
 app.get("/", (req, res) => {
-  res.se
+  res.json({
+    message: "Hello World",
+  });
+});
+
+// Crud Operation For adding Student Data in Database
+
+//Create
+app.post("/student", (req, res) => {
+  res.json({
+    message: "Data Created Successfully",
+  });
+});
+
+//Read
+app.fetch("/student", (req, res) => {
+  res.json({
+    message: "Data Fetched Successfully",
+  });
+});
+
+//Update
+app.put("/student:id", (req, res) => {
+  res.json({
+    message: "Data Updated Successfully",
+  });
+});
+
+//Delete
+
+app.delete("/student:id", (req, res) => {
+  res.json({
+    message: "Data Deleted Successfully",
+  });
 });
 
 app.listen(port, () => {
